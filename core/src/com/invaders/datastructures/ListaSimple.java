775ff992fd70e259b2baf209c4cddb3469403e1d
Jose Ortega <ortega.josant@gmail.com>
2018-03-26 17:41:21 -0600

    Agregados las listas enlazadas, también se agregó LibGDX para trabajar la parte gráfica

---
+++ core/src/com/invaders/datastructures/ListaSimple.java
@@ -0,0 +1,115 @@
+package com.invaders.datastructures;
+
+
+public class ListaSimple<T> {
+    private NodoSimple<T> first;
+    private int length;
+
+    public ListaSimple() {
+
+    }
+
+    public void add(NodoSimple<T> nodo) {
+        if (isEmpty()) {
+            add(0, nodo);
+        } else {
+            NodoSimple<T> temp = first;
+            for (int i = 0; i < length; i++) {
+                if (temp.getNext() == null) {
+                    temp.setNext(nodo);
+                    this.length += 1;
+                    break;
+                } else {
+                    temp = temp.getNext();
+                }
+            }
+        }
+    }
+
+    public void add(int index, NodoSimple<T> nodo) {
+        if (index == 0) {
+            nodo.setNext(first);
+            first = nodo;
+            length += 1;
+        } else {
+            if (index >= getlength()) {
+                throw new IndexOutOfBoundsException("Index out of range");
+            } else {
+                NodoSimple<T> prev = first;
+                NodoSimple<T> temp = first;
+                int i = 0;
+                while (i <= index) {
+                    if (i == index) {
+                        nodo.setNext(temp);
+                        prev.setNext(nodo);
+                        length += 1;
+                        break;
+                    } else {
+                        prev = temp;
+                        temp = temp.getNext();
+                        i += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    public void remove(int index) {
+        if (index == 0) {
+            first = first.getNext();
+            length -= 1;
+        } else {
+            NodoSimple<T> prev = first;
+            NodoSimple<T> temp = first;
+
+            int i = 0;
+            while (index < getlength()) {
+                if (i == index) {
+                    prev.setNext(temp.getNext());
+                    this.length -= 1;
+                } else if (i < index) {
+                    prev = temp;
+                    temp = temp.getNext();
+                    i += 1;
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+
+    public boolean remove(NodoSimple<T> nodo) {
+        NodoSimple<T> prev = first;
+        NodoSimple<T> temp = first;
+        for (int i = 0; i < length; i++) {
+            if (temp == nodo) {
+                prev.setNext(temp.getNext());
+                this.length -= 1;
+                return true;
+            } else {
+                prev = temp;
+                temp = temp.getNext();
+                i += 1;
+            }
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return this.first == null;
+
+    }
+
+    public int getlength() {
+        return length;
+    }
+
+    public void print() {
+        NodoSimple<T> temp = first;
+        for (int i = 0; i < getlength(); i++) {
+            System.out.println(temp.getDato());
+            temp = temp.getNext();
+        }
+    }
+}
+
---
+++ core/src/com/jose/datastructures/ListaDoble.java
@@ -0,0 +1,124 @@
+package dataStructures;
+
+public class ListaDoble<T> {
+    private NodoDoble<T> first;
+    private int length;
+
+    public ListaDoble() {
+
+    }
+
+    public void add(NodoDoble<T> nodo) {
+        if (isEmpty()) {
+            addFirst(nodo);
+        } else {
+            NodoDoble<T> temp = first;
+            for (int i = 0; i < length; i++) {
+                if (temp.getNext() == null) {
+                    nodo.setPrevious(temp);
+                    temp.setNext(nodo);
+                    this.length += 1;
+                    break;
+                }
+                temp = temp.getNext();
+            }
+        }
+    }
+
+    public void add(int index, NodoDoble<T> nodo) {
+        if (index == 0) {
+            if (isEmpty()) {
+                addFirst(nodo);
+            } else {
+                nodo.setNext(first);
+                first.setPrevious(nodo);
+                first = nodo;
+                length += 1;
+            }
+        } else {
+            if (index >= length) {
+                throw new IndexOutOfBoundsException("Index out of range");
+            } else {
+                NodoDoble<T> prev = first;
+                NodoDoble<T> temp = first;
+                int i = 0;
+                while (i <= index) {
+                    if (i == index) {
+                        nodo.setNext(temp);
+                        nodo.setPrevious(prev);
+                        prev.setNext(nodo);
+                        length += 1;
+                        break;
+                    } else {
+                        prev = temp;
+                        temp = temp.getNext();
+                        i += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    private void addFirst(NodoDoble<T> nodo) {
+        first = nodo;
+        length += 1;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void print() {
+        NodoDoble<T> temp = first;
+        for (int i = 0; i < length; i++) {
+            System.out.println(temp.getDato());
+            temp = temp.getNext();
+        }
+    }
+
+    public void remove(NodoDoble<T> nodo) {
+        if (nodo == first) {
+            first = first.getNext();
+            first.setPrevious(null);
+            length -= 1;
+        } else {
+            NodoDoble<T> prev = first;
+            NodoDoble<T> temp = first;
+            for (int i = 0; i < length; i++) {
+                if (nodo == temp) {
+                    prev.setNext(temp.getNext());
+                    temp.getNext().setNext(prev);
+                    length -= 1;
+                    break;
+                }
+                prev = temp;
+                temp = temp.getNext();
+            }
+        }
+    }
+
+    public void remove(int index) {
+        if (index == 0) {
+            first = first.getNext();
+            first.setPrevious(null);
+            length -= 1;
+        } else {
+            NodoDoble<T> prev = first;
+            NodoDoble<T> temp = first;
+            int i = 0;
+            while (index < length) {
+                if (i == index) {
+                    prev.setNext(temp.getNext());
+                    temp.getNext().setPrevious(prev);
+                    length -= 1;
+                    break;
+                } else {
+                    prev = temp;
+                    temp = temp.getNext();
+                    i += 1;
+                }
+            }
+        }
+    }
+}
+

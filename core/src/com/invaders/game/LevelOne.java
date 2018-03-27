3861be85833e26d7d8ce0c7df9f158fd295d6042
Jose Ortega <ortega.josant@gmail.com>
2018-03-27 15:31:07 -0600

    Revert "Revert "Se agregó la clase bala, ahora la nave puede disparar""
    This reverts commit 297e8e2724524591ef72de74af662f8b0d03eca3.

---
+++ core/src/com/invaders/game/LevelOne.java
@@ -0,0 +1,87 @@
+package com.invaders.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.invaders.datastructures.ListaSimple;
+import com.invaders.datastructures.NodoSimple;
+import com.invaders.logic.Bullet;
+import com.invaders.logic.Enemy;
+import com.invaders.logic.Nave;
+
+public class LevelOne extends Window {
+	Nave player;
+	Enemy enemy;
+	ListaSimple<Bullet> bullets;
+
+	public LevelOne(InvadersLauncher invadersLauncher) {
+		super(invadersLauncher);
+		bullets = new ListaSimple<>();
+	}
+
+	@Override
+	public void show() {
+		player = new Nave();
+		enemy = new Enemy(0, new Texture("images/enemy1.png"));
+	}
+
+	@Override
+	public void render(float delta) {
+		renderGame();
+		doAction();
+	}
+
+	public void renderGame() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		invadersLauncher.batch.begin();
+		for (int i = 0; i < bullets.getlength(); i++) {
+			bullets.find(i).render(invadersLauncher.batch);
+		}
+		player.render(invadersLauncher.batch);
+		enemy.render(invadersLauncher.batch);
+
+		invadersLauncher.batch.end();
+	}
+
+	public void doAction() {
+		
+		boolean rigth = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+		boolean left = Gdx.input.isKeyPressed(Input.Keys.LEFT);
+		boolean space = Gdx.input.isKeyJustPressed(Input.Keys.SPACE);
+
+		float delta = Gdx.graphics.getDeltaTime();
+
+		if (rigth)
+			player.setXCoord(player.getXCoord() + (player.getSpeed() * delta));
+		else if (left)
+			player.setXCoord(player.getXCoord() - (player.getSpeed() * delta));
+		else if (space || (space && rigth) || (space && left))
+			bullets.add(new NodoSimple<Bullet>(new Bullet(player.getXCoord() + 12)));
+
+		for (int i = 0; i < bullets.getlength(); i++) {
+			bullets.find(i).update(delta);
+			if (bullets.find(i).getRemove()) {
+				bullets.remove(bullets.find(i));
+			}
+			
+
+		}
+
+//		for (int i = 0; i < bullets.getlength(); i++) {
+//
+//			if (bullets.find(i).getRemove()) {
+//				bullets.remove(bullets.find(i));
+//				System.out.println("eliminando 1");
+//				System.out.println(bullets.getlength());
+//			}
+//		}
+	}
+
+	@Override
+	public void dispose() {
+		invadersLauncher.batch.dispose();
+
+	}
+}
